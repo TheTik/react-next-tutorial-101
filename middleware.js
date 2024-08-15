4bd1158638520f06077ed6a80f5057c762be18e8
TINV-021\AppDev <piya_ph@toptier.co.th>
2024-08-15 22:43:53 +0700

    Save ...

--- middleware.js
+++ middleware.js
@@ -6,39 +6,60 @@ import { jwtVerify, importJWK } from 'jose'
 export async function middleware(request) {
     console.log("##### [middlware] To do something before call API #####");
 
+    // Test Signature verified.
+    // try {
+    //     const crackSecretJWK = {
+    //         kty: 'oct',
+    //         k: 'AppDev' // Replace with your actual base64 encoded secret key
+    //     }
 
+    //     let crackToken = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IkdhcnJpY2suTW9ocjgxQGhvdG1haWwuY29tIiwiaWF0IjoxNzIzNzM0OTY4LCJleHAiOjE3MjM3Mzg1Njh9.e2x8NldNP0E06oQI-SQvbg3XP70RnBHbq4rg5SK-6eU"
+    //     const crackSecretKey = await importJWK(crackSecretJWK, 'HS256')
+    //     const { payload } = await jwtVerify(crackToken, crackSecretKey)
+    //     console.log("----------------------------------------------------------------------------------------");
+    //     console.log("√ Signature verified. ");
+    //     console.log("----------------------------------------------------------------------------------------");
+    //     console.log("Payload : ", payload.email);
+    //     console.log("----------------------------------------------------------------------------------------");
+    // } catch (error) {
+    //     console.log("----------------------------------------------------------------------------------------");
+    //     console.log("!!! Invalid signature. !!!");
+    //     console.log("----------------------------------------------------------------------------------------");
+    //     console.log('Error : ', error)
+    //     console.log("----------------------------------------------------------------------------------------");
+    // }
 
     // Check JWT authorize 
     try {
+
         let token = request.cookies.get('cookieAuth')
-        if(typeof token === "undefined") return NextResponse.next();
+        if (typeof token === "undefined") return NextResponse.next();
 
         const secretJWK = {
             kty: 'oct',
             k: process.env.JOSE_SECRET // Replace with your actual base64 encoded secret key
         }
 
-        console.log('token : ', token.value)
-        console.log('secretJWK : ', secretJWK)
+        //console.log('token : ', token.value)
+        //console.log('secretJWK : ', secretJWK)
 
         const secretKey = await importJWK(secretJWK, 'HS256')
         const { payload } = await jwtVerify(token.value, secretKey)
+
         const requestHeaders = new Headers(request.headers)
         requestHeaders.set('user', JSON.stringify({ email: payload.email }))
 
         const response = NextResponse.next({
-            request:{
+            request: {
                 headers: requestHeaders,
             },
         });
         return response;
 
     } catch (error) {
-        console.log('error', error)
+        //console.log('error', error)
         return NextResponse.redirect(new URL('/Error500', request.url))
     }
-
-
 
     const nextUrl = request.nextUrl
     //console.log("nextUrl : ", nextUrl);
@@ -51,5 +72,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-    matcher: ["/:path*","/getUsers/:path*", '/getUser/:path*']
+    matcher: ["/:path*", "/getUsers/:path*", '/getUser/:path*']
 };
